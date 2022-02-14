3c589240e868d8609c6e776a7a8a50695825bed2
Daniel Zolman <zolman.daniel@gmail.com>
2022-02-14 13:11:32 -0700

    added updated db design

--- app.py
+++ app.py
@@ -5,7 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+   return render_template('home.html')
+
+
+@app.route('/dashboard')
+def dashboard():
    return render_template('dashboard.html')
+
+
+@app.route('/currencies')
+def currencies():
+   return render_template('cryptocurrencies.html')
+
+
+@app.route('/watchlist')
+def watchlist():
+   return render_template('watchlist.html')
+
+
+@app.route('/transactions')
+def transactions():
+   return render_template('transactions.html')
 
 
 if __name__ == '__main__':
